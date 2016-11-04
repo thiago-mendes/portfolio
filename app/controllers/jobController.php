fc28ff2a2a31ec2663cdf8ea5aaf8e2af09a3b89
Thiago Mendes <almeida16tm@gmail.com>
2016-11-04 00:17:48 -0200

    Functions na controller - CRUD

--- app/controllers/jobController.php
+++ app/controllers/jobController.php
@@ -1,31 +1,43 @@
 <?php
 
 class Job extends Controller {
+	private $model;
+
+	public function __construct() {
+		$this->model = new Jobs_Model();
+	}
+
+
 	public function index() {
-
-		$db = new Jobs_Model();
-
-		print_r($db->delete('jobs', 'id=1'));
-
-		/*
-		$db->update('jobs', array(
-		"nome" => "NOVO NOME mais e mais",
-		"url"  => "www.teste.com",
-		), 'id=1');
-		 */
-
-		//print_r($db->read('jobs', 'id=1'));
-
-		/*$db->insert('jobs', array(
-		"nome" => "Crane job 01",
-		"url"  => "link 00001",
-		));*/
-
 		//$this->view('jobs/index');
 	}
 
-	public function crane() {
-		$this->view('jobs/crane');
+	public function listarJobs() {		
+		print_r($this->model->read('jobs'));
+	}
+
+	public function adicionarJob() {
+		$this->model->insert('jobs', array(
+			"nome" => "Crane job 01",
+			"url"  => "link 00001",
+		));
+	}
+
+	public function deletarJob() {
+		$url = explode("/", $_GET[key]);
+		$id = $url[2];
+		
+		print_r($this->model->delete('jobs', 'id='.$id));
+	}
+
+	public function atualizarJob() {
+		$url = explode("/", $_GET[key]);
+		$id = $url[2];
+
+		$this->model->update('jobs', array(
+		"nome" => "NOVO NOME mais e mais",
+		"url"  => "www.teste.com",
+		), 'id=1');
 	}
 }
 
--- system/model.php
+++ system/model.php
@@ -3,7 +3,7 @@ class Model {
 	protected $db;
 
 	public function __construct() {
-		$this->db = new PDO("mysql:host=127.0.0.1;dbname=portfolio", 'root', 'cr@ne');
+		$this->db = new PDO("mysql:host=127.0.0.1;dbname=new-portfolio", 'root', 'mendes15');
 	}
 
 	public function insert($tabela, Array $dados) {
